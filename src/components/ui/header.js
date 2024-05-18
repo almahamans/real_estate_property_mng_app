f5b3455a423a97d860d5d13f317ff3283430914d
Almaha <mahaw999@gmail.com>
2024-05-18 22:39:18 +0300

    update the code

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -9,13 +9,13 @@ return(
     <div>
       <BrowserRouter>
        <header className='p-5 font-bold text-md text-teal-950 flex flex-row flex-nowrap gap-5 justify-center items-center bg-teal-400 '>
-       <span><Link to='/'>Home</Link></span>
+       <span><Link to='real_estate_property_mng_app/'>Home</Link></span>
        <span><Link to='/list-properties'>List properties</Link></span>
        <span><Link to='/add-properties'>Add properties</Link></span>    
       </header>
 
       <Routes>
-        <Route exact path="/" element={<Main />} />
+        <Route exact path="real_estate_property_mng_app/" element={<Main />} />
         <Route path="/list-properties" element={<DisplayFunction />} />
         <Route path="/add-properties" element={<Add />} />
         <Route path="/show-property" element={<DisplayProperty />} />
